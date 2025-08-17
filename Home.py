169e7d342ca04898d952e8b7e147948956130e56
joetaro736 <joequegyra@gmail.com>
2025-08-16 22:11:55 -0300

    Add files via upload

--- Home.py
+++ Home.py
@@ -8,11 +8,13 @@ st.sidebar.title("""Ciberbullying
                  6°B""")
 st.sidebar.image('download.png')
 
-tab1, tab2 = st.tabs(['aonde acontece', 'como acontece'])
+tab1, tab2, tab3 = st.tabs(['oque é' ,'aonde acontece', 'como acontece'])
 with tab1:
+    st.subheader('Cyberbullying é bullying online, acontece através de redes sociais, mensagens ou e-mails, que pode incluir ameaças, insultos, difamação ou compartilhamento de conteúdo constrangedor. Pode afetar a autoestima e a saúde mental das vítimas.')
+with tab2:
     st.subheader("""O cyberbullying acontece em ambientes virtuais como redes sociais, aplicativos de mensagem, plataformas de jogos e celulares. É um tipo de agressão que utiliza a internet para intimidar, assustar ou envergonhar.
 """)
-with tab2:
+with tab3:
     tab1, tab2, tab3, tab4, tab5 = st.tabs(['Fóruns Online', 'Criação de Perfil Falso', 'Redes Sociais', 'Jogos Online', 'E-mails'])
     with tab1:
         st.subheader("""Publicação de comentários maldosos ou difamatórios. """)
